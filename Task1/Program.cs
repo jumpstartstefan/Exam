2345b7ad9d5dff9b909f80e38ccb7d2575e850d0
jumpstartstefan <stefan_ruse@mail.bg>
2014-10-13 15:04:17 +0300

    reformat
    delete namespace

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,28 +1,23 @@
 ﻿using System;
 
-
-namespace Task1
+class Program
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.Write("N=");
-            int number = Int32.Parse(Console.ReadLine());
+	static void Main()
+	{
+		Console.Write("N=");
+		int number = Int32.Parse(Console.ReadLine());
+		
+		for (int i = 1; i <= number; i++)
+		{
 
-            
-            for (int i = 1; i < number+1; i++)
-            {
+			for (int j = 1; j <= number; j++)
+			{
+				if (i == j)
+					continue;
 
-
-                for (int j = 1; j < number+1; j++)
-                {
-                    if (i == j)
-                        continue;
-
-                    Console.WriteLine("{0} {1}", i, j);
-                }
-            }
-        }
-    }
+				Console.WriteLine("{0} {1}", i, j);
+			}
+		}
+	}
 }
+
