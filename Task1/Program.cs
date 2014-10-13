e363efe642249b8ae024fe50ed13d35945bd7bb2
jumpstartstefan <stefan_ruse@mail.bg>
2014-10-13 14:54:50 +0300

    taski1
    task1 form jumpstart exam

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N=");
+            int number = Int32.Parse(Console.ReadLine());
+
+            
+            for (int i = 1; i < number+1; i++)
+            {
+
+
+                for (int j = 1; j < number+1; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    Console.WriteLine("{0} {1}", i, j);
+                }
+            }
+        }
+    }
+}
