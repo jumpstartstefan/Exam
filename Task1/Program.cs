0f84ff22998ae6b4eab9242bad3c492ad26dec74
jumpstartstefan <stefan_ruse@mail.bg>
2014-10-13 14:59:09 +0300

    del
    delete string args

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,7 +5,7 @@ namespace Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Write("N=");
             int number = Int32.Parse(Console.ReadLine());
